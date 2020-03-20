5212cd70f4fa28870a510ee04c65e2554a32e632
fullStackOasis <fullstackdev@fullstackoasis.com>
2020-03-20 10:24:04 -0400

    Add styling and some documentation.

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 /**
  * Simple demo of https://www.npmjs.com/package/react-native-sound
+ * The app plays an included mp3 file when the user clicks on 'Play' button.
+ * It stops the playback when the user clicks on the 'Stop' button.
  */
 import React from 'react';
 import {
@@ -35,24 +37,44 @@ class App extends React.Component {
 	render() {
 		return (
 		<View style={styles.body}>
-			<Text style={styles.sectionTitle}>Click to Play</Text>
-			<Button onPress={this.onPlayPress.bind(this)} title="Play" />
-			<Text style={styles.sectionTitle}>Click to Stop</Text>
-			<Button onPress={this.onStopPress.bind(this)} title="Stop" />
+			<Text style={styles.sectionTitle}>Click button to Play</Text>
+			<View style={styles.buttonStyle}>
+				<Button style={styles.buttonStyle}
+					onPress={this.onPlayPress.bind(this)}
+					title="Play"
+				/>
+			</View>
+			<Text style={styles.sectionTitle}>Click button to Stop</Text>
+			<View style={styles.buttonStyle}>
+				<Button buttonStyle={styles.buttonStyle}
+					onPress={this.onStopPress.bind(this)}
+					title="Stop"
+				/>
+			</View>
 		</View>
-		);
+    	);
 	}
 };
 
 const styles = StyleSheet.create({
 	body: {
 		backgroundColor: Colors.white,
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		backgroundColor: 'gainsboro'
 	},
 	sectionTitle: {
 		fontSize: 24,
 		fontWeight: '600',
 		color: Colors.black,
+		padding: 20,
+		backgroundColor: 'gainsboro' /* See https://reactnative.dev/docs/colors */,
 	},
+	buttonStyle: {
+		padding: 20,
+		backgroundColor: 'gainsboro' /* See https://reactnative.dev/docs/colors */,
+	}
 });
 
 export default App;
